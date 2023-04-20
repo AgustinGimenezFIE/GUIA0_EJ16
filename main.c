79854f8616cb8ef975ce29be370b5070c9e42372
Agustin <darkstone19@gmail.com>
2023-04-19 21:56:21 -0300

    Primer commit

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-
+/*
+ EJERCICIO 16 : Defina una función que tome por parámetro dos punteros que apuntan a cadenas de caracteres, y retorna el resultado de concatenar ambas.
+ */
 int main() {
     printf("Hello, World!\n");
     return 0;
